7d0b08e1f2d41ef0af2c816075c52535d4fb10d9
colecrum26 <cwcrumrine@gmail.com>
2023-05-15 16:36:02 -0500

    knocked out game functionality

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -1,27 +1,12 @@
-// import { useState } from "react";
-// import Board from "./components/gameBoard";
+import Board from "./components/gameBoard";
 import "./App.css";
-import "./components/board.css";
+import "./boardStyle.css";
 
 function App() {
-  let [p1Score, setp1Score] = useState(0);
-  let [p2Score, setp2Score] = useState(0);
-  let [draws, setDraws] = useState(0);
-
-  function handleScore(winner) {
-    if (p1 == winner) {
-      setp1Score(p1Score + 1);
-    } else if (p2 == winner) {
-      setp2Score(p2Score + 1);
-    } else {
-      setDraws(draws + 1);
-    }
-  }
 
   return (
     <div className="App">
-      <div id="vert">vert</div>
-      <div id="zont">zont</div>
+      <Board />
     </div>
   );
 }
