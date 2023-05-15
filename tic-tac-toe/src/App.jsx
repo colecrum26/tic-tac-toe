630cc6ef2a5187b2a20ab499b0d9aac3cd00682d
colecrum26 <cwcrumrine@gmail.com>
2023-05-15 12:57:04 -0500

    formatted and uncommented

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -1,22 +1,22 @@
 // import { useState } from "react";
 // import Board from "./components/gameBoard";
-import './App.css';
-import './components/board.css';
+import "./App.css";
+import "./components/board.css";
 
 function App() {
-  // let [p1Score, setp1Score] = useState(0);
-  // let [p2Score, setp2Score] = useState(0);
-  // let [draws, setDraws] = useState(0);
-  
-  // function handleScore(winner) {
-  //   if (p1 == winner) {
-  //     setp1Score(p1Score+1);
-  //   } else if (p2 == winner) {
-  //     setp2Score(p2Score+1);
-  //   } else {
-  //     setDraws(draws+1);
-  //   }
-  // }
+  let [p1Score, setp1Score] = useState(0);
+  let [p2Score, setp2Score] = useState(0);
+  let [draws, setDraws] = useState(0);
+
+  function handleScore(winner) {
+    if (p1 == winner) {
+      setp1Score(p1Score + 1);
+    } else if (p2 == winner) {
+      setp2Score(p2Score + 1);
+    } else {
+      setDraws(draws + 1);
+    }
+  }
 
   return (
     <div className="App">
