72420923dfcc27e12751ca4df97b2652ae2ecb0d
colecrum26 <cwcrumrine@gmail.com>
2023-05-15 12:38:02 -0500

    added scoring functions and started on board

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -1,10 +1,25 @@
+import { useState } from "react";
+import Board from "./components/gameBoard";
 import './App.css';
 
 function App() {
+  let [p1Score, setp1Score] = useState(0);
+  let [p2Score, setp2Score] = useState(0);
+  let [draws, setDraws] = useState(0);
+  
+  function handleScore(winner) {
+    if (p1 == winner) {
+      setp1Score(p1Score+1);
+    } else if (p2 == winner) {
+      setp2Score(p2Score+1);
+    } else {
+      setDraws(draws+1);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      < Board />
     </div>
   );
 }
---
+++ tic-tac-toe/src/components/gameBoard.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+const boardStart = [];
+boardStart.push({
+});
+
+function Board(props) {
+    const [moves, setMoves] = useState[boardStart];
+
+    function handleMove()
+}
+
+export default Board;
