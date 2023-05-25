e191e4103d5d4c8f9f39bab5b6aaa00bea0f6adc
colecrum26 <cwcrumrine@gmail.com>
2023-05-25 12:43:56 -0500

    added player name update

--- tic-tac-toe/src/components/startScreen.jsx
+++ tic-tac-toe/src/components/startScreen.jsx
@@ -1,60 +1,70 @@
+import { useState } from "react";
+import GameBoard from "./gameBoard";
 // allows players to name themselves (with default Player1/2) and be used during game
 // button-click starts the game after players are named
 
-{/* <Square value={squares[0]} onSquareClick={() => handleClick(0)}/> */}
+// let player1 = window.prompt("Enter Player 1");
+// let player2 = window.prompt("Enter Player 2");
 
-import { useState } from "react";
-import GameBoard from "./gameBoard";
+// setPlayers([player1, player2]);
 
+function Players({ player1 }) {
+  return (
+    <div>
+      {player1 && (
+        <>
+          <span>{player1}</span>
 
-function Players({player1}, {player2}) {
-    return (
-        <div>
-        <span>{player1}</span>
-        <span>{player2}</span>
-        </div>
-    )
+        </>
+      )}
+    </div>
+  );
 }
 
-function StartBtn(props) {
-    return (
-        <button id="startBtn" onClick={onStartClick}>
-            Start Game
-        </button>
-    )
-}
+// function StartBtn(props) {
+//     return (
+//         <button id="startBtn" onClick={onStartClick}>
+//             Start Game
+//         </button>
+//     )
+// }
 
 export default function Start() {
-    const [player1, setplayer1] = useState("");
-    const [player2, setplayer2] = useState("");
+    const [player1, setPlayer1] = useState("");
+    const [player2, setPlayer2] = useState("");
+  const [players, setPlayers] = useState([]);
 
-    function handlePlayer () {
-        
-    }
+  function handlePlayer(event) { 
+    console.log(event);
+    event.preventDefault();
+    setPlayers([...players, event.target.value]);
+    console.log(players);
+  }
 
-    function handleStart() {
-        return (
-            <div>
-                <GameBoard />
-            </div>
-        )
-    }
+  // function handleStart() {
+  //     return (
+  //         <div>
+  //             <GameBoard />
+  //         </div>
+  //     )
+  // }
 
-    return (
-        <div>
-        <form >
-            <input type="text" name="player1" placeholder="Player 1" />
-            <button type="submit" id="player1" onClick={handlePlayer}>Enter</button>
-            <input type="text" name="player2" placeholder="Player 2"/>
-            <button type="submit" id="player2" onClick={handlePlayer}>Enter</button>
-        </form>
-        <div>
-            <Players />
-        </div>
-        <div>
-            <StartBtn onStartClick={() => handleStart()}/>
-        </div>
-        </div>
-
-    )
+  return (
+    <div>
+      <form>
+        <input type="text" name="player1" placeholder="Player 1" onChange={event => setPlayer1(event.target.value)} />
+        <button type="submit" id="player1" onClick={handlePlayer}>
+          Enter
+        </button>
+        <input type="text" name="player2" placeholder="Player 2" />
+        <button type="submit" id="player2" onClick={handlePlayer}>
+          Enter
+        </button>
+      </form>
+      <div>
+        <Players player1={player1}/>
+      </div>
+      <div>{/* <StartBtn onStartClick={() => handleStart()}/> */}</div>
+    </div>
+  );
 }
